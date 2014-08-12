d891b4be690b41b6d3246359f875a276d9b83cb6
Ilja Bauer <i.bauer@its-bh.de>
2014-08-12 11:10:36 +0200

    Added run.sh to configure db connection

--- Dockerfile
+++ Dockerfile
@@ -7,7 +7,8 @@ RUN apt-get update
 RUN apt-get install -y wget default-jre
 RUN mkdir /opt/teamcity
 RUN wget -q -O - http://download-ln.jetbrains.com/teamcity/TeamCity-$TEAM_CITY_VERSION.tar.gz | tar xzf - -C /opt/teamcity
+Add run.sh /run
 
 VOLUME ["/opt/teamcity-data"]
 EXPOSE 8111
-CMD /opt/teamcity/TeamCity/bin/teamcity-server.sh run
+CMD /run
---
+++ run.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+if [ -n "$DB_HOST" ]; then
+	CONFIG_PATH = $TEAMCITY_DATA_PATH/config/database.properties
+	touch $CONFIG_PATH
+
+	echo "connectionUrl=jdbc:postgresql://$DB_HOST/$DB_NAME" > $CONFIG_PATH
+	echo "connectionProperties.user=$DB_USER" >> $CONFIG_PATH
+	echo "connectionProperties.password=$DB_PASSWORD" >> $CONFIG_PATH
+fi
+
+/opt/teamcity/TeamCity/bin/teamcity-server.sh run
