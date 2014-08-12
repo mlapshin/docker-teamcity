49d30b7ac54dcde1ae9f4fdfd7b55256c5289d8b
Ilja Bauer <i.bauer@its-bh.de>
2014-08-12 12:22:24 +0200

    Bug fix

--- run.sh
+++ run.sh
@@ -3,9 +3,9 @@ if [ -n "$DB_HOST" ]; then
 	CONFIG_PATH = $TEAMCITY_DATA_PATH/config/database.properties
 	touch $CONFIG_PATH
 
-	echo "connectionUrl=jdbc:postgresql://$DB_HOST/$DB_NAME" > $CONFIG_PATH
-	echo "connectionProperties.user=$DB_USER" >> $CONFIG_PATH
-	echo "connectionProperties.password=$DB_PASSWORD" >> $CONFIG_PATH
+	echo connectionUrl=jdbc:postgresql://$DB_HOST/$DB_NAME > $CONFIG_PATH
+	echo connectionProperties.user=$DB_USER >> $CONFIG_PATH
+	echo connectionProperties.password=$DB_PASSWORD >> $CONFIG_PATH
 fi
 
 cp /tmp/$JDBC_NAME $TEAMCITY_DATA_PATH/lib/jdbc/$JDBC_NAME
